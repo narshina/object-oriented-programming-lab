b0f32b143c3a7ab0246f036c3f4be37faee624a4
narshina <116632617+narshina@users.noreply.github.com>
2023-08-21 10:10:54 +0530

    Add files via upload

---
+++ arithmeticpackage/main.java
@@ -0,0 +1,19 @@
+import arithmetic.*;
+class Main63
+{
+public static void main(String args[])
+{
+arithmetic.Sum a1=new arithmetic.Sum();
+arithmetic.Subtract s1=new arithmetic.Subtract();
+arithmetic.Product p1=new arithmetic.Product();
+arithmetic.Division d1=new arithmetic.Division();
+a1.get();
+a1.show();
+s1.get();
+s1.show();
+p1.get();
+p1.show();
+d1.get();
+d1.show();
+}
+}
---
+++ arithmeticpackage/substract.java
@@ -0,0 +1,24 @@
+package arithmetic;
+import java.util.Scanner;
+interface A2
+{
+void get();
+void show();
+}
+public class Subtract implements A2
+{
+int a,b,dif;
+public void get()
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a:");
+a=sc.nextInt();
+System.out.println("Enter b:");
+b=sc.nextInt();
+}
+public void show()
+{
+dif=a-b;
+System.out.println("The difference is:" +dif);
+}
+}
